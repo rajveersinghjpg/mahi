a9cb6771db51380d8d2c6bd940060c1abd538dff
rajveersinghjpg <prepwithraj@gmail.com>
2025-10-15 22:22:47 +0530

    Update main.js

--- script/main.js
+++ script/main.js
@@ -39,10 +39,10 @@ document.addEventListener("DOMContentLoaded", function () {
     "December",
   ];
 
-  const targetDay = 7;
-  const targetMonthIndex = 11; // December
-  const targetYear = 2024;
-  const birthYear = 2001;
+  const targetDay = 16;
+  const targetMonthIndex = 10; // December
+  const targetYear = 2025;
+  const birthYear = 2005;
 
   let currentDay = 1;
   let currentMonthIndex = 0;
