8d76aa8e69e943e56d72ad9a5d4ed806ecf2b54e
rajveersinghjpg <prepwithraj@gmail.com>
2025-10-15 22:57:51 +0530

    Update main.js

--- script/main.js
+++ script/main.js
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function () {
   ];
 
   const targetDay = 16;
-  const targetMonthIndex = 10; // December
+  const targetMonthIndex = 09; // December
   const targetYear = 2025;
   const birthYear = 2005;
 
